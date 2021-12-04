675771f56e6a8b99a57dd4bd654426cfd1693468
Anhad <84476084+07anhad@users.noreply.github.com>
2021-12-04 18:22:57 +0530

    not working

--- main.js
+++ main.js
@@ -1,4 +1,30 @@
 window.addEventListener("scroll", function () {
     const header = this.document.querySelector("header");
     header.classList.toggle("sticky", window.scrollY > 0);
+});
+
+const serviceModals = document.querySelectorAll(".service-modal")
+const learnmoreBtns = document.querySelectorAll(".learn-more-btn")
+const modalCloseBtns = document.querySelectorAll(".modal-close-btn")
+
+var modal = function (modalClick) {
+    serviceModals[modalClick].classList.add("active");
+
+}
+
+learnmoreBtns.forEach((learnmoreBtns, i) => {
+    learnmoreBtns.addEventListener("click", () => {
+        modal[i];
+    })
 })
+
+modalCloseBtns.forEach((modalCloseBtns) => {
+    modalCloseBtns.addEventListener("click", () => {
+        serviceModals.forEach((modalView) => {
+            modalView.classList.remove("active");
+             
+        });
+    });
+});
+
+
